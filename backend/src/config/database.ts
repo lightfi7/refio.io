51ccd659b09353981b7ea126d3a621db15081791
Fi <158217226+lightfi7@users.noreply.github.com>
2024-11-24 09:29:47 -0800

    Update database.ts

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -2,7 +2,11 @@ import mongoose from 'mongoose';
 
 const connectDB = async (): Promise<void> => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
+    await mongoose.connect(process.env.MONGODB_URI as string, {
+      authSource: "admin",
+      user: "devman",
+      pass: "mari2Ana23sem",
+    });
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
