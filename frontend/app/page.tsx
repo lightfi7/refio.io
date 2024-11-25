97478c651982e9be6d876bf3a1abd3c1381430b5
Fi <158217226+lightfi7@users.noreply.github.com>
2024-11-25 10:37:25 -0800

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <section className="relative flex flex-col items-center justify-center rounded-xl gap-4 py-8 md:py-10"
     >
+      <img src="/images/bg.png" className="absolute backdrop-blur-sm bg-transparent rounded-xl opacity-10 md:opacity-90" alt="hero" />
       <section className="relative isolate pt-8" id="hero" >
         <div className="py-24 lg:pb-40">
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
@@ -18,7 +19,6 @@ export default function Home() {
                       className="h-5 w-5"
                       fill="currentColor"
                       viewBox="0 0 20 20"
-                      x-description="Heroicon name: solid/chevron-right"
                       xmlns="http://www.w3.org/2000/svg"
                     >
                       <path
